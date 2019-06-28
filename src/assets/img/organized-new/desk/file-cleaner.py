0f67032c2a0f8e4807dc1bead5ee3198ace0206f
Brenner Pacelli <brenner_pacelli@hotmail.com>
2019-06-28 20:33:18 -0300

    Created create-defs.py

--- src/assets/img/organized-new/desk/file-cleaner.py
+++ src/assets/img/organized-new/desk/file-cleaner.py
@@ -3,14 +3,7 @@ import re
 import ntpath
 import os
 
-clean='icon'
-
-iconPath='1-raw/icon/'
-logoPath='1-raw/logo/'
-
-pathToSave='2-clean/'
-
-def create_save_dir(dirName):
+def create_save_dir(pathToSave, dirName):
   try:
     os.mkdir(pathToSave + dirName)
     print('Directory ' + dirName + ' created!')
@@ -30,40 +23,47 @@ def remove_all_array(array, str):
     str = remove_all(substr, str)
   return str
 
-create_save_dir('icon')
-for root, dirs, files in os.walk(iconPath):
-  for svgFile in files:
-    if svgFile.endswith(".svg"):
-      svgFilePath = os.path.join(root, svgFile)
-      with open(svgFilePath) as svg:
-        fullSvg = svg.read()
+def routine():
+  iconPath='all/icon/'
+  logoPath='all/logo/'
 
-        fileName = ntpath.basename(svgFile)
-        cleanedFilePath = pathToSave + 'icon/' + str(fileName)
-        cleanedFile = open(cleanedFilePath, "w")
+  pathToSave='clean/'
+  create_save_dir(pathToSave, '')
+  create_save_dir(pathToSave, 'icon')
 
-        cleanedSvg = remove_all_array([
-          ' fill="none"',
-          ' fill-rule="evenodd"',
-          ' clip-rule="evenodd"',
-          ' fill="black"'
-        ], fullSvg)
+  for root, dirs, files in os.walk(iconPath):
+    for svgFile in files:
+      if svgFile.endswith(".svg"):
+        svgFilePath = os.path.join(root, svgFile)
+        with open(svgFilePath) as svg:
+          fullSvg = svg.read()
 
-        cleanedFile.write(cleanedSvg)
-        cleanedFile.close()
+          fileName = ntpath.basename(svgFile)
+          cleanedFilePath = pathToSave + 'icon/' + str(fileName)
+          cleanedFile = open(cleanedFilePath, "w")
 
-create_save_dir('logo')
-for root, dirs, files in os.walk(logoPath):
-  for svgFile in files:
-    print(svgFile)
-    if svgFile.endswith(".svg"):
-      svgFilePath = os.path.join(root, svgFile)
-      with open(svgFilePath) as svg:
-        fullSvg = svg.read()
+          cleanedSvg = remove_all_array([
+            ' fill="none"',
+            ' fill-rule="evenodd"',
+            ' clip-rule="evenodd"',
+            ' fill="black"'
+          ], fullSvg)
 
-        fileName = ntpath.basename(svgFile)
-        cleanedFilePath = pathToSave + 'logo/' + str(fileName)
-        cleanedFile = open(cleanedFilePath, "w")
+          cleanedFile.write(cleanedSvg)
+          cleanedFile.close()
 
-        cleanedFile.write(fullSvg)
-        cleanedFile.close()
+  create_save_dir(pathToSave,'logo')
+  for root, dirs, files in os.walk(logoPath):
+    for svgFile in files:
+      print(svgFile)
+      if svgFile.endswith(".svg"):
+        svgFilePath = os.path.join(root, svgFile)
+        with open(svgFilePath) as svg:
+          fullSvg = svg.read()
+
+          fileName = ntpath.basename(svgFile)
+          cleanedFilePath = pathToSave + 'logo/' + str(fileName)
+          cleanedFile = open(cleanedFilePath, "w")
+
+          cleanedFile.write(fullSvg)
+          cleanedFile.close()
--- src/assets/img/organized-new/desk/svg-creator.py
+++ src/assets/img/organized-new/desk/svg-creator.py
@@ -3,13 +3,6 @@ import re
 import ntpath
 import os
 
-iconPath='3-compressed/icon/'
-logoPath='3-compressed/logo/'
-defs = open("defs.svg", "w")
-
-defs.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n')
-defs.write('\t<defs>\n')
-
 def create_save_dir(dirName):
   try:
     os.mkdir(pathToSave + dirName)
@@ -17,69 +10,85 @@ def create_save_dir(dirName):
   except:
     print('Directory ' + dirName + ' icon already exists!')
 
-create_save_dir('icon')
-for root, dirs, files in os.walk(iconPath):
-  for svgFile in files:
-    print(svgFile)
-    if svgFile.endswith(".svg"):
-      svgFilePath = os.path.join(root, svgFile)
-      with open(svgFilePath) as svg:
-        fullSvg = svg.read()
+def justify_string(string):
+  string_length = len(string)+1
+  string_revised = string.rjust(string_length)
+  return string_revised
 
-        fileName = ntpath.basename(svgFile).split('.svg')[0]
+def routine():
+  iconPath='clean/icon/'
+  logoPath='clean/logo/'
+  defs = open("defs.svg", "w")
 
-        pathStart = fullSvg.find('>') + 1
-        pathEnd = fullSvg.find('</svg>')
+  defs.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">\n')
+  defs.write('\t<defs>\n')
 
-        svgPath = fullSvg[pathStart : pathEnd]
+  create_save_dir('icon')
+  for root, dirs, files in os.walk(iconPath):
+    for svgFile in files:
+      print(svgFile)
+      if svgFile.endswith(".svg"):
+        svgFilePath = os.path.join(root, svgFile)
+        with open(svgFilePath) as svg:
+          fullSvg = svg.read()
 
-        path = ''.join(['\t\t<symbol id="', fileName, '" viewBox="0 0 24 24">', svgPath, '</symbol>\n'])
-        defs.write(path)
+          fileName = ntpath.basename(svgFile).split('.svg')[0]
 
-create_save_dir('logo')
-for root, dirs, files in os.walk(logoPath):
-  for svgFile in files:
-    print(svgFile)
-    if svgFile.endswith(".svg"):
-      svgFilePath = os.path.join(root, svgFile)
-      with open(svgFilePath) as svg:
-        fullSvg = svg.read()
+          pathStart = fullSvg.find('>') + 1
+          pathEnd = fullSvg.find('</svg>')
 
-        fileName = ntpath.basename(svgFile).split('.svg')[0]
+          svgPath = fullSvg[pathStart : pathEnd]
 
-        pathStart = fullSvg.find('>') + 1
-        pathEnd = fullSvg.find('</svg>')
+          path = ''.join(['\t\t<symbol id="', fileName, '" viewBox="0 0 72 72">', svgPath, '</symbol>\n'])
+          defs.write(path)
 
-        svgAtributes = fullSvg[0 : pathStart]
-        width = re.search("(width=\"[0-9]+\"){1}", svgAtributes)
-        height = re.search("(height=\"[0-9]+\"){1}", svgAtributes)
-        viewbox = re.search("(viewbox=\"[0-9]+\s+\"){1}", svgAtributes)
-        print(width)
-        print(height)
-        print(viewbox)
-        if width is not None and len(width) >= 1:
-          width = " ".join(width)
-        else:
-          width = ""
+  create_save_dir('logo')
+  for root, dirs, files in os.walk(logoPath):
+    for svgFile in files:
+      print(svgFile)
+      if svgFile.endswith(".svg"):
+        svgFilePath = os.path.join(root, svgFile)
+        with open(svgFilePath) as svg:
+          fullSvg = svg.read()
 
-        if height is not None and len(height) >= 1:
-          height = " ".join(height)
-        else:
-          height = ""
+          fileName = ntpath.basename(svgFile).split('.svg')[0]
 
-        if viewbox is not None and len(viewbox) >= 1:
-          viewbox = " ".join(viewbox)
-        else:
-          viewbox = ""
+          pathStart = fullSvg.find('>') + 1
+          pathEnd = fullSvg.find('</svg>')
+
+          svgAtributes = fullSvg[0 : pathStart]
+          width = re.search("(width=\"[0-9]+\"){1}", svgAtributes)
+          height = re.search("(height=\"[0-9]+\"){1}", svgAtributes)
+          viewbox = re.search("(viewbox=\"[0-9]+\s+\"){1}", svgAtributes)
+          widthString = ''
+          heightString = ''
+          viewboxString = ''
+          print(width)
+          print(height)
+          print(viewbox)
+          if width is not None and len(width.group(0)) >= 1:
+            widthString = justify_string(width.group(0))
+          else:
+            widthString = ""
+
+          if height is not None and len(height.group(0)) >= 1:
+            heightString = justify_string(height.group(0))
+          else:
+            heightString = ""
+
+          if viewbox is not None and len(viewbox.group(0)) >= 1:
+            viewboxString = justify_string(viewbox.group(0))
+          else:
+            viewboxString = ""
 
 
 
-        svgPath = fullSvg[pathStart : pathEnd]
+          svgPath = fullSvg[pathStart : pathEnd]
 
-        path = ''.join(['\t\t<symbol id="', fileName, width, height, viewbox, ">", svgPath, '</symbol>\n'])
-        defs.write(path)
+          path = ''.join(['\t\t<symbol id="', fileName, '"', widthString, heightString, viewboxString, ">", svgPath, '</symbol>\n'])
+          defs.write(path)
 
-defs.write('\t</defs>\n')
-defs.write('</svg>\n')
+  defs.write('\t</defs>\n')
+  defs.write('</svg>\n')
 
-defs.close()
+  defs.close()
